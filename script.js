eb079c18a6608dc84ebeb14c97b41ed0c4e62fed
Koustubha068 <KoustuK@gmail.com>
2024-10-21 13:46:51 -0400

    beginning of JSON and AJAX

---
+++ script.js
@@ -0,0 +1,18 @@
+let thePets = [
+    {
+    "name": "Meowsalot",
+    "species": "cat",
+    "favFood": "tuna"
+    },
+    {
+    "name": "Barky",
+    "species": "dog",
+    "favFood": "carrots"
+    }
+]
+/* To access: 
+
+thePets[1].favFood 
+
+ This combines JSON. basically combines arrays and objects. JSON combines objects and arrays in javascript
+ So in this we are calling the array thePets first. Then getting the 1st thing in the array. From the 1st position taking the 3rd object called FavFood */
